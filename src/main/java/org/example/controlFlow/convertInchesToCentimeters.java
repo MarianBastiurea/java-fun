9787959e2b053cb27404669a66ad926ebc510962
MarianBastiurea <marian.bastiurea@gmail.com>
2024-02-04 17:33:34 +0000

    convert height from inches in cm or from feet and inches in cm.

---
+++ src/main/java/org/example/controlFlow/convertInchesToCentimeters.java
@@ -0,0 +1,8 @@
+package org.example.controlFlow;
+
+public class convertInchesToCentimeters {
+    public static double inchesToCm(int heightInInches){
+        double heightInCm=heightInInches*2.54;
+        return heightInCm;
+    }
+}
---
+++ src/test/java/org/example/controlFlow/convertFeetAndInchesToCmTest.java
@@ -0,0 +1,16 @@
+package org.example.controlFlow;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class convertFeetAndInchesToCmTest {
+    @Test
+    public void given5feetAnd8InchesShouldReturn172Point72Cm() {
+        int heightInFeet = 5;
+        int heightInInches = 8;
+        double heightInCm = 172.72;
+        double result = convertFeetAndInchesToCm.feetAndInchesToCm(heightInFeet, heightInInches);
+        assertEquals(heightInCm, result);
+    }
+}
