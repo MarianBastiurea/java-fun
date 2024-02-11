f90f3a380092c3155cfdba1b3f38a34dfa5ece5c
MarianBastiurea <marian.bastiurea@gmail.com>
2024-02-11 07:05:14 +0000

    Create a class Customer.
    Create three Constructor  for this class.
    First one with all three fields(name, credit limit and email) should assign the arguments directly to the instance field;
    Second one, no args constructor that calls another constructor, passing some literal value for each argument
    Last one, will have onle name and email as args, and calls another constructor

---
+++ src/test/java/org/example/c04/inheritance/CustomerTest.java
@@ -0,0 +1,22 @@
+package org.example.c04.inheritance;
+
+import java.util.concurrent.Callable;
+
+public class CustomerTest {
+    public static void main(String[] args) {
+        Customer customer = new Customer("Marian", 1000, "myEmail");
+        System.out.println(customer.getName());
+        System.out.println((customer.getCreditLimit()));
+        System.out.println(customer.getCustomerEmail());
+
+        Customer secondCustomer = new Customer();
+        System.out.println(secondCustomer.getName());
+        System.out.println((secondCustomer.getCreditLimit()));
+        System.out.println(secondCustomer.getCustomerEmail());
+
+        Customer thirdCustomer = new Customer("Joe", "Joe@email.com");
+        System.out.println(thirdCustomer.getName());
+        System.out.println((thirdCustomer.getCreditLimit()));
+        System.out.println(thirdCustomer.getCustomerEmail());
+    }
+}
