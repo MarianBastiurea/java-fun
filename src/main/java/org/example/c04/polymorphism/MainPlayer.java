9118f26dfa37ae9f9c110cbd11ae5299cfa713b9
MarianBastiurea <marian.bastiurea@gmail.com>
2024-02-18 19:10:33 +0000

    Add a encapsulated class EnhancedPlayer
    Player it's a unprotected class.

---
+++ src/main/java/org/example/c04/polymorphism/MainPlayer.java
@@ -0,0 +1,11 @@
+package org.example.c04.polymorphism;
+
+public class MainPlayer {
+    public static void main(String[] args) {
+
+        EnhancedPlayer tim = new EnhancedPlayer("Tim", 200,
+                "Sword");
+        System.out.println("Initial health is " + tim.healthRemaining());
+
+    }
+}
