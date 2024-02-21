4ea79fd3a33baeda7e2b14f68230503fa5aa6e01
MarianBastiurea <marian.bastiurea@gmail.com>
2024-02-21 18:06:26 +0000

    add ArrayList class type Grocery list

--- src/main/java/org/example/c06ArrayList/GroceryItem.java
+++ src/main/java/org/example/c06ArrayList/GroceryItem.java
@@ -9,7 +9,7 @@ record GroceryItem(String name, String type, int count) {
         this(name, "DAIRY", 1);
     }
 
-    @Override
+   @Override
     public String toString() {
         return String.format("%d %s in %s", count, name.toUpperCase(), type);
     }
