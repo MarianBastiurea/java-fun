85ae1e0379655c044534409f6a2724cb36e77f07
MarianBastiurea <marian.bastiurea@gmail.com>
2024-02-28 15:07:54 +0000

    check if an ArrayList is empty

---
+++ src/main/java/org/example/c06/ArrayList/IsEmpty.java
@@ -0,0 +1,25 @@
+package org.example.c06.ArrayList;
+
+import java.util.ArrayList;
+
+/*
+Write a Java program to test whether an array list is empty or not.
+ */
+public class IsEmpty {
+    public static void main(String[] args) {
+        ArrayList<Integer> numbers = new ArrayList<>();
+        ArrayList<Integer> numbers1 = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            numbers.add(i);
+        }
+        System.out.println("ArrayList is " + numbers + " ");
+        int size=numbers.size();
+        int size1=numbers1.size();
+        if(size>0)
+        {System.out.println("Size of ArrayList numbers is: "+size);
+        } else System.out.println("ArrayList is empty");
+        if(size1>0)
+        {System.out.println("Size of ArrayList numbers1 is: "+size1);
+        } else System.out.println("ArrayList numbers1 is empty");
+    }
+}
