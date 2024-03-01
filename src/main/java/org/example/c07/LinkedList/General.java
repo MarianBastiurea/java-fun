ba13c8364a4e7a3daf224690be3d6a5d45991b40
MarianBastiurea <marian.bastiurea@gmail.com>
2024-03-01 09:27:26 +0000

    general LinkedList class
    join two list, remove elements, shuffle and swap elements from list

--- src/main/java/org/example/c07/LinkedList/General.java
+++ src/main/java/org/example/c07/LinkedList/General.java
@@ -1,27 +1,29 @@
 package org.example.c07.LinkedList;
 
 
-import java.util.Collection;
+import java.util.Collections;
 import java.util.LinkedList;
 
 public class General {
     public static void main(String[] args) {
         LinkedList<String> list = new LinkedList<>();
-        /*list.add("Red");
+        list.add("Red");
         list.add("Orange");
         list.add("Yellow");
         list.add("Green");
         list.add("Blue");
         list.add("Indigo");
         list.add("Violet");
-        System.out.println(" Original" + list);*/
-        buildList();
+        System.out.println(" Original" + list);
         firstAndLastOccurrenceOfASpecifiedElement(list);
         displayElementsAndTheirPosition(list);
+        swapElements(list, "Red", "Violet");
+        shuffle(list);
+        joinTwoLists(list, list);
         removeElementsAtSpecifiedPosition(list, 2);
         removeFirstAndLastElements(list);
         removeAllElements(list);
-        swapElements(list, "Red", "Violet");
+
     }
 
     public static LinkedList buildList() {
@@ -38,9 +40,8 @@ public class General {
     }
 
     public static void firstAndLastOccurrenceOfASpecifiedElement(LinkedList list) {
-
         //Write a Java program to get the first and last occurrence of the specified elements in a linked list.
-        buildList();
+
         list.add("Red");
         System.out.println("First occurrence of red is a position " + list.indexOf("Red"));
         System.out.println("Last occurrence of red is a position " + list.lastIndexOf("Red"));
@@ -80,12 +81,31 @@ public class General {
 
     public static void swapElements(LinkedList list, String s1, String s2) {
         //Write a Java program that swaps two elements in a linked list.
+        System.out.println("List before swapping" + list);
         String temp = "";
         int i = list.indexOf(s1);
         int j = list.indexOf(s2);
+        list.remove(i);
         list.add(i, s2);
+        list.remove(j);
         list.add(j, s1);
         System.out.println("New list after swapping " + s1 + " with " + s2);
         System.out.println(list);
+    }
+
+    public static void shuffle(LinkedList list) {
+        //Write a Java program to shuffle elements in a linked list.
+        Collections.shuffle(list);
+        System.out.println("New list after shuffle all elements is");
+        System.out.println(list);
+    }
+
+    public static void joinTwoLists(LinkedList list1, LinkedList list2) {
+        LinkedList<String> newList = new LinkedList<>();
+        newList.addAll(list1);
+        newList.addAll(list2);
+        System.out.println("New list after joining is");
+        System.out.println(newList);
+
     }
 }
