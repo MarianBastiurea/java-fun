8d88b9926bf7c80d4552acf4a3295508b9a0d8df
MarianBastiurea <marian.bastiurea@gmail.com>
2024-03-03 07:35:18 +0000

    Create Shape1 an Interface type class
    Use Shape1 to calculate Rectangle area

---
+++ src/main/java/org/example/c08/Abstraction/MainShape1.java
@@ -0,0 +1,8 @@
+package org.example.c08.Abstraction;
+
+public class MainShape1 {
+    public static void main(String[] args) {
+        Rectangle rectangle = new Rectangle(12, 14);
+        System.out.println("Rectangle area is: " + rectangle.getArea());
+    }
+}
