8442fc3d8943a2a47f14a14bb39eca61ed442f6d
MarianBastiurea <marian.bastiurea@gmail.com>
2024-03-06 05:28:17 +0000

    update QueryItem

--- src/main/java/org/example/c09/Generics/QueryItem.java
+++ src/main/java/org/example/c09/Generics/QueryItem.java
@@ -2,5 +2,5 @@ package org.example.c09.Generics;
 
 public interface QueryItem {
 
-    public boolean matchFieldValue(String fieldName, String value);
+     boolean matchFieldValue(String fieldName, String value);
 }
