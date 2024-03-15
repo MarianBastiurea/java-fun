01bdf5e96975af463676a4f5858084e3b956deb7
MarianBastiurea <marian.bastiurea@gmail.com>
2024-03-15 17:43:50 +0000

    changing algorithm and add Transaction class

--- src/main/java/org/example/c13/Final/BankAccount.java
+++ src/main/java/org/example/c13/Final/BankAccount.java
@@ -1,12 +1,18 @@
 package org.example.c13.Final;
 
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class BankAccount {
+
     public enum AccountType {CHECKING, SAVINGS}
 
     private final AccountType accountType;
-    private final double balance;
+    private double balance;
+    private final Map<Long, Transaction> transactions = new LinkedHashMap<>();
 
-    public BankAccount(AccountType accountType, double balance) {
+    BankAccount(AccountType accountType, double balance) {
         this.accountType = accountType;
         this.balance = balance;
     }
@@ -19,8 +25,26 @@ public class BankAccount {
         return balance;
     }
 
+    public Map<Long, String> getTransactions() {
+
+        Map<Long, String> txMap = new LinkedHashMap<>();
+        for (var tx : transactions.entrySet()) {
+            txMap.put(tx.getKey(), tx.getValue().toString());
+        }
+        return txMap;
+    }
+
     @Override
     public String toString() {
         return "%s $%.2f".formatted(accountType, balance);
+    }
+
+    void commitTransaction(int routingNumber, long transactionId,
+                           String customerId, double amount) {
+
+        balance += amount;
+        transactions.put(transactionId,
+                new Transaction(routingNumber, transactionId,
+                        Integer.parseInt(customerId), amount));
     }
 }
--- src/main/java/org/example/c13/Final/BankCustomer.java
+++ src/main/java/org/example/c13/Final/BankCustomer.java
@@ -5,13 +5,14 @@ import java.util.Arrays;
 import java.util.List;
 
 public class BankCustomer {
+
     private static int lastCustomerId = 10_000_000;
 
     private final String name;
     private final int customerId;
     private final List<BankAccount> accounts = new ArrayList<>();
 
-    public BankCustomer(String name, double checkingAmount, double savingsAmount) {
+    BankCustomer(String name, double checkingAmount, double savingsAmount) {
         this.name = name;
         this.customerId = lastCustomerId++;
         accounts.add(new BankAccount(BankAccount.AccountType.CHECKING, checkingAmount));
@@ -22,8 +23,22 @@ public class BankCustomer {
         return name;
     }
 
+    public String getCustomerId() {
+        return "%015d".formatted(customerId);
+    }
+
     public List<BankAccount> getAccounts() {
-        return new ArrayList<>(accounts);
+        return List.copyOf(accounts);
+    }
+
+    public BankAccount getAccount(BankAccount.AccountType type) {
+
+        for (var account : accounts) {
+            if (account.getAccountType() == type) {
+                return account;
+            }
+        }
+        return null;
     }
 
     @Override
