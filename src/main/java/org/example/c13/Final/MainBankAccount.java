ec2fd2bffad407bbc86f95fd17ab0a3a2f8dc7a8
MarianBastiurea <marian.bastiurea@gmail.com>
2024-03-15 07:39:45 +0000

    create BankAccount class

---
+++ src/main/java/org/example/c13/Final/MainBankAccount.java
@@ -0,0 +1,21 @@
+package org.example.c13.Final;
+
+import java.util.List;
+
+public class MainBankAccount {
+    public static void main(String[] args) {
+
+//        BankAccount account =
+//                new BankAccount(BankAccount.AccountType.CHECKING, 500);
+//        System.out.println(account);
+
+        BankCustomer joe = new BankCustomer("Joe", 500.00,
+                10000.00);
+        System.out.println(joe);
+
+        List<BankAccount> accounts = joe.getAccounts();
+        accounts.clear();
+        System.out.println(joe);
+
+    }
+}
