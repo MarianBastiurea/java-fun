1e31ead9c7a1682b0a48a954c4d032a4cd95e924
MarianBastiurea <marian.bastiurea@gmail.com>
2024-03-21 21:48:41 +0000

    update

--- src/main/java/org/example/c01/expression/convertInchesToCentimeters.java
+++
@@ -1,8 +0,0 @@
-package org.example.c01.expression;
-
-public class convertInchesToCentimeters {
-    public static double inchesToCm(int heightInInches){
-        double heightInCm=heightInInches*2.54;
-        return heightInCm;
-    }
-}
