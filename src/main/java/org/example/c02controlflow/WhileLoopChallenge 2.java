974c27aff9cad0c0bc6bd9c1b30e4d2e939f36f4
MarianBastiurea <marian.bastiurea@gmail.com>
2024-03-21 21:51:54 +0000

    refactoring

---
+++ src/main/java/org/example/c02controlflow/WhileLoopChallenge 2.java
@@ -0,0 +1,28 @@
+package org.example.c02controlflow;
+
+public class WhileLoopChallenge {
+    public static boolean isEven(int number) {
+        if (number < 0) {
+            return false;
+        } else return number % 2 == 0;
+    }
+    public static int[] storeEven(int start, int end) {
+        int count=0;
+        int[] temporaryArray=new int[end-start+1];
+        int i=start;
+            while ( i <= end) {
+                if (isEven(i)) {
+                    temporaryArray[count]=i;
+                    count++;
+                }
+                i++;
+            }
+            int[] resultArray=new int[count];
+        int j=0;
+            while(j<count){
+                resultArray[j]=temporaryArray[j];
+                j++;
+            }
+        return resultArray;
+    }
+}
