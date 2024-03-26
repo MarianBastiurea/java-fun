a086b1976179ca35f384d4ad438e7bc0109c4cee
MarianBastiurea <marian.bastiurea@gmail.com>
2024-03-26 06:32:33 +0000

    read student data and writing in a file

---
+++ src/main/java/org/example/c17inputoutput/StudentDemographics.java
@@ -0,0 +1,14 @@
+package org.example.c17inputoutput;
+
+public record StudentDemographics(String countryCode, int enrolledMonth,
+                                  int enrolledYear, int ageAtEnrollment, String gender,
+                                  boolean previousProgrammingExperience ) {
+
+    @Override
+    public String toString() {
+        return "%s,%d,%d,%d,%s,%b".formatted(countryCode,
+                enrolledMonth,enrolledYear, ageAtEnrollment,gender,
+                previousProgrammingExperience);
+    }
+}
+
