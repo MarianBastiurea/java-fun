7e2d9885e4377abe695d75e473c72d5913e174c7
MarianBastiurea <marian.bastiurea@gmail.com>
2024-03-31 18:45:12 +0100

    delete some exercises

--- src/main/java/org/example/test1/Sum1.java
+++
@@ -1,9 +0,0 @@
-package org.example.test1;
-
-public class Sum1 {
-
-    public void sum1(int number1, int number2){
-        int sum=number1+number2;
-         System.out.println("Sum of "+number1+" + "+number2+" is "+sum);
-    }
-}
--- src/main/java/org/example/test1/Sum2.java
+++
@@ -1,8 +0,0 @@
-package org.example.test1;
-
- class Sum2 {
-    public void sum2(int number1, int number2){
-        int sum=number1+number2;
-        System.out.println("Sum of "+number1+" + "+number2+" is "+sum);
-    }
-}
