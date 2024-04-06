c765a57e8a3cb9bc315581bc87d79c9e43735b71
MarianBastiurea <marian.bastiurea@gmail.com>
2024-04-06 10:37:51 +0100

    calling public class from main class

---
+++ src/main/java/org/example/c19namingconvention/MainSeries.java
@@ -0,0 +1,18 @@
+package org.example.c19namingconvention;
+
+public class MainSeries {
+    public static void main(String[] args) {
+        for(int i=0; i <=10; i++) {
+            System.out.println(Series.nSum(i));
+        }
+
+        System.out.println("*****************************");
+        for(int i=0; i<=10; i++) {
+            System.out.println(Series.factorial(i));
+        }
+        System.out.println("*****************************");
+        for(int i=0; i<=10; i++) {
+            System.out.println(Series.fibonacci(i));
+        }
+    }
+}
