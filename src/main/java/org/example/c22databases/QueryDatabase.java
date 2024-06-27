b4a68270b4c0ca5fb85b305019810454a05d14f6
MarianBastiurea <marian.bastiurea@gmail.com>
2024-06-27 12:16:41 +0100

    update algorithm

--- src/main/java/org/example/c22databases/QueryDatabase.java
+++ src/main/java/org/example/c22databases/QueryDatabase.java
@@ -1,5 +1,7 @@
 package org.example.c22databases;
 
+import com.mysql.cj.jdbc.MysqlDataSource;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -12,57 +14,57 @@ import java.util.Properties;
 public class QueryDatabase {
 
 
-        public static void main(String[] args) {
+    public static void main(String[] args) {
 
-            Properties props = new Properties();
-            try {
-                props.load(Files.newInputStream(Path.of("music.properties"),
-                        StandardOpenOption.READ));
-            } catch (IOException e) {
-                throw new RuntimeException(e);
+        Properties props = new Properties();
+        try {
+            props.load(Files.newInputStream(Path.of("music.properties"),
+                    StandardOpenOption.READ));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        String albumName = "Tapestry";
+        String query = "SELECT * FROM music.albumview WHERE album_name='%s'"
+                .formatted(albumName);
+
+        var dataSource = new MysqlDataSource();
+        dataSource.setServerName(props.getProperty("serverName"));
+        dataSource.setPort(Integer.parseInt(props.getProperty("port")));
+        dataSource.setDatabaseName(props.getProperty("databaseName"));
+
+        try (var connection = dataSource.getConnection(
+                props.getProperty("user"),
+                System.getenv("MYSQL_PASS"));
+             Statement statement = connection.createStatement();
+        ) {
+            ResultSet resultSet = statement.executeQuery(query);
+
+            var meta = resultSet.getMetaData();
+            for (int i = 1; i <= meta.getColumnCount(); i++) {
+                System.out.printf("%d %s %s%n",
+                        i,
+                        meta.getColumnName(i),
+                        meta.getColumnTypeName(i)
+                );
             }
+            System.out.println("===================");
 
-            String albumName = "Tapestry";
-            String query = "SELECT * FROM music.albumview WHERE album_name='%s'"
-                    .formatted(albumName);
+            for (int i = 1; i <= meta.getColumnCount(); i++) {
+                System.out.printf("%-15s", meta.getColumnName(i).toUpperCase());
+            }
+            System.out.println();
 
-            var dataSource = new MysqlDataSource();
-            dataSource.setServerName(props.getProperty("serverName"));
-            dataSource.setPort(Integer.parseInt(props.getProperty("port")));
-            dataSource.setDatabaseName(props.getProperty("databaseName"));
-
-            try (var connection = dataSource.getConnection(
-                    props.getProperty("user"),
-                    System.getenv("MYSQL_PASS"));
-                 Statement statement = connection.createStatement();
-            ) {
-                ResultSet resultSet = statement.executeQuery(query);
-
-                var meta = resultSet.getMetaData();
+            while (resultSet.next()) {
                 for (int i = 1; i <= meta.getColumnCount(); i++) {
-                    System.out.printf("%d %s %s%n",
-                            i,
-                            meta.getColumnName(i),
-                            meta.getColumnTypeName(i)
-                    );
-                }
-                System.out.println("===================");
-
-                for (int i = 1; i <= meta.getColumnCount(); i++) {
-                    System.out.printf("%-15s", meta.getColumnName(i).toUpperCase());
+                    System.out.printf("%-15s", resultSet.getString(i));
                 }
                 System.out.println();
-
-                while (resultSet.next()) {
-                    for (int i = 1; i <= meta.getColumnCount(); i++) {
-                        System.out.printf("%-15s", resultSet.getString(i));
-                    }
-                    System.out.println();
-                }
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
             }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
     }
-
 }
+
+
--- src/main/java/org/example/c22databases/SelectFirst10artists.java
+++ src/main/java/org/example/c22databases/SelectFirst10artists.java
@@ -1,5 +1,7 @@
 package org.example.c22databases;
 
+import com.mysql.cj.jdbc.MysqlDataSource;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -9,28 +11,29 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Properties;
 
+
 public class SelectFirst10artists {
 
-        public static void main(String[] args) {
+    public static void main(String[] args) {
 
-            Properties props = new Properties();
-            try {
-                props.load(Files.newInputStream(Path.of("music.properties"),
-                        StandardOpenOption.READ));
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
+        Properties props = new Properties();
+        try {
+            props.load(Files.newInputStream(Path.of("music.properties"),
+                    StandardOpenOption.READ));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
-            var dataSource = new MysqlDataSource();
-            dataSource.setServerName(props.getProperty("serverName"));
-            dataSource.setPort(Integer.parseInt(props.getProperty("port")));
-            dataSource.setDatabaseName(props.getProperty("databaseName"));
-            try {
-                dataSource.setMaxRows(10);
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
-            String query = "SELECT * FROM music.artists limit 10";
+        var dataSource = new MysqlDataSource();
+        dataSource.setServerName(props.getProperty("serverName"));
+        dataSource.setPort(Integer.parseInt(props.getProperty("port")));
+        dataSource.setDatabaseName(props.getProperty("databaseName"));
+        try {
+            dataSource.setMaxRows(10);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        String query = "SELECT * FROM music.artists limit 10";
 
 //        String query = """
 //            WITH RankedRows AS (
@@ -42,32 +45,30 @@ public class SelectFirst10artists {
 //                                FROM RankedRows
 //                            WHERE row_num <= 10""";
 
-            try (var connection = dataSource.getConnection(
-                    props.getProperty("user"),
-                    System.getenv("MYSQL_PASS"));
-                 Statement statement = connection.createStatement();
-            ) {
-                ResultSet resultSet = statement.executeQuery(query);
+        try (var connection = dataSource.getConnection(
+                props.getProperty("user"),
+                System.getenv("MYSQL_PASS"));
+             Statement statement = connection.createStatement();
+        ) {
+            ResultSet resultSet = statement.executeQuery(query);
 
-                var meta = resultSet.getMetaData();
+            var meta = resultSet.getMetaData();
 
-                System.out.println("===================");
+            System.out.println("===================");
 
+            for (int i = 1; i <= meta.getColumnCount(); i++) {
+                System.out.printf("%-15s", meta.getColumnName(i).toUpperCase());
+            }
+            System.out.println();
+
+            while (resultSet.next()) {
                 for (int i = 1; i <= meta.getColumnCount(); i++) {
-                    System.out.printf("%-15s", meta.getColumnName(i).toUpperCase());
+                    System.out.printf("%-15s", resultSet.getString(i));
                 }
                 System.out.println();
-
-                while (resultSet.next()) {
-                    for (int i = 1; i <= meta.getColumnCount(); i++) {
-                        System.out.printf("%-15s", resultSet.getString(i));
-                    }
-                    System.out.println();
-                }
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
             }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
     }
-
 }
