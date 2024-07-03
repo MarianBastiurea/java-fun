5dfc953f5b68bc81416f96a8400c19447d05e8e1
MarianBastiurea <marian.bastiurea@gmail.com>
2024-07-03 09:21:03 +0100

    update WebContent

--- src/main/java/org/example/c23network/WebContent.java
+++ src/main/java/org/example/c23network/WebContent.java
@@ -1,78 +1,44 @@
 package org.example.c23network;
 
-import java.net.MalformedURLException;
-import java.net.URI;
-import java.net.URISyntaxException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.URL;
-
+import java.net.URLConnection;
 
 public class WebContent {
-        public static void main(String[] args) {
 
-            URI baseSite = URI.create("https://learnprogramming.academy");
-            URI timsSite = URI.create(
-                    "courses/complete-java-masterclass");
-            print(timsSite);
+    public static void main(String[] args) {
 
-            try {
-                URI uri = new URI(
-                        "http://user:pw@store.com:5000/products/phones?os=android#samsung");
-                print(uri);
-
-                URI masterClass = baseSite.resolve(timsSite);
-                URL url = masterClass.toURL();
-                System.out.println(url);
-                print(url);
-            } catch (URISyntaxException | MalformedURLException e) {
-                throw new RuntimeException(e);
-            }
-        }
-
-        private static void print(URI uri) {
-
-            System.out.printf("""            
-                ----------------------------------------------
-                [scheme:]scheme-specific-part[#fragment]
-                ----------------------------------------------
-                Scheme: %s
-                Scheme-specific part: %s
-                  Authority: %s
-                    User info: %s
-                    Host: %s
-                    Port: %d
-                    Path: %s
-                    Query: %s
-                Fragment: %s
-                """,
-                    uri.getScheme(),
-                    uri.getSchemeSpecificPart(),
-                    uri.getAuthority(),
-                    uri.getUserInfo(),
-                    uri.getHost(),
-                    uri.getPort(),
-                    uri.getPath(),
-                    uri.getQuery(),
-                    uri.getFragment()
-            );
-        }
-
-        private static void print(URL url) {
-
-            System.out.printf("""            
-                ----------------------------------------------
-                  Authority: %s
-                    User info: %s
-                    Host: %s
-                    Port: %d
-                    Path: %s
-                    Query: %s
-                """,
-                    url.getAuthority(),
-                    url.getUserInfo(),
-                    url.getHost(),
-                    url.getPort(),
-                    url.getPath(),
-                    url.getQuery()
-            );
+        try {
+//            URL url = new URL("http://example.com");
+            URL url = new URL("https://jsonplaceholder.typicode.com/todos?id=5");
+//            printContents(url.openStream());
+            URLConnection urlConnection = url.openConnection();
+            System.out.println(urlConnection.getContentType());
+            urlConnection.getHeaderFields()
+                    .entrySet()
+                    .forEach(System.out::println);
+            System.out.println(urlConnection.getHeaderField("Cache-Control"));
+            urlConnection.connect();
+            printContents(urlConnection.getInputStream());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
+
+    private static void printContents(InputStream is) {
+
+        try (BufferedReader inputStream = new BufferedReader(
+                new InputStreamReader(is))
+        ) {
+            String line;
+            while ((line = inputStream.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
